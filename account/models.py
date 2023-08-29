da72de09f41fb11aced854119f29d904666e2fe3
Orozbek Zhenishbek uulu <88781324+orozDev@users.noreply.github.com>
2023-08-29 15:47:00 +0600

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from django.db import models
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django_resized import ResizedImageField
 from phonenumber_field.modelfields import PhoneNumberField
 from .managers import UserManager
 from utils.models import TimeStampAbstractModel
+from uuid import uuid4
 
 
 class User(AbstractUser):
