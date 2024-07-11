0bddb38b2c536d77a6edd13fae99d1143689dd3f
Orozbek Zhenishbek uulu <88781324+orozDev@users.noreply.github.com>
2024-07-11 15:51:35 +0600

    Update models.py

--- account/models.py
+++ account/models.py
@@ -11,20 +11,17 @@ from uuid import uuid4
 
 
 class User(AbstractUser):
-    
     class Meta:
         verbose_name = _('пользователь')
         verbose_name_plural = _('пользователи')
         ordering = ('-date_joined',)
-        
-        
-    avatar = ResizedImageField(size=[500, 500], crop=['middle', 'center'],
-        upload_to='avatars/', force_format='WEBP', quality=90, verbose_name=_('аватарка'), null=True, blank=True) 
-    phone = PhoneNumberField(max_length=100, unique=True, verbose_name=_('номер телефона'))
-    email = models.EmailField(blank=True, verbose_name=_('электронная почта'), unique=True)
-    last_activity = models.DateTimeField(blank=True, 
-        null=True, verbose_name=_('последнее действие'),)
-  
+
+    avatar = ResizedImageField(_('аватарка'), size=[500, 500], crop=['middle', 'center'],
+                               upload_to='avatars/', force_format='WEBP', quality=90, null=True, blank=True)
+    phone = PhoneNumberField(_('номер телефона'), unique=True, null=True)
+    email = models.EmailField(_('электронная почта'), blank=True, null=True, unique=True)
+    last_activity = models.DateTimeField(_('последнее действие'), blank=True, null=True)
+
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
@@ -33,23 +30,27 @@ class User(AbstractUser):
     @property
     def get_full_name(self):
         return f'{self.last_name} {self.first_name}'
-    
-    get_full_name.fget.short_description = _('полное имя')    
+
+    get_full_name.fget.short_description = _('полное имя')
 
     def __str__(self):
         return f'{self.get_full_name or str(self.phone)}'
 
 
-class UserResetPassword(TimeStampAbstractModel):
+def password_reset_key_expire_default():
+    return timezone.now() + timezone.timedelta(days=settings.EXPIRE_DAYS)
 
+
+class UserResetPassword(TimeStampAbstractModel):
     class Meta:
         verbose_name = _('Ключ для сброса пароля')
         verbose_name_plural = _('Ключи для сброса пароля')
         ordering = ('-created_at', '-updated_at')
 
-    user = models.OneToOneField('account.User', on_delete=models.CASCADE, verbose_name=_('пользователь'))
+    user = models.OneToOneField('account.User', on_delete=models.CASCADE, related_name='rest_password',
+                                verbose_name=_('пользователь'))
     key = models.UUIDField(_('ключ'), default=uuid4, editable=False)
-    expire_date = models.DateTimeField(_('срок действия'), default=timezone.now() + timezone.timedelta(days=settings.EXPIRE_DAYS))
+    expire_date = models.DateTimeField(_('срок действия'), default=password_reset_key_expire_default)
 
     def __str__(self):
         return f'{self.user}'
